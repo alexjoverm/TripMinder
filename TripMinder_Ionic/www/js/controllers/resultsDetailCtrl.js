5752a68095ca1805df50abab58c7210bfcce543c
Alex Jover <alexjovermorales@gmail.com>
2015-03-17 13:00:56 +0100

    Improved performance on render

--- TripMinder_Ionic/www/js/controllers/resultsDetailCtrl.js
+++ TripMinder_Ionic/www/js/controllers/resultsDetailCtrl.js
@@ -1,13 +1,32 @@
 angular.module('tripminder')
 
-.controller('ResultsDetailCtrl', ['$scope', '$stateParams', '$timeout', 'DataSvc', 'MapsSvc',
-  function($scope, $stateParams, $timeout, DataSvc, MapsSvc) { 
+.controller('ResultsDetailCtrl', ['$scope', '$stateParams', '$timeout', '$ionicScrollDelegate', 'DataSvc', 'MapsSvc',
+  function($scope, $stateParams, $timeout, $ionicScrollDelegate, DataSvc, MapsSvc) {
 
       $scope.route = DataSvc.searchResults[$stateParams.id][$stateParams.num];
       $scope.adress = DataSvc.adress;
 
       $scope.map = MapsSvc.CreateDefaultResultMap();
-      
+
+      $scope.config = {
+          itemsDisplayed: 15
+      };
+
+      $scope.moreItemsCanBeAdded = function(){
+          return $scope.route.steps.length > $scope.config.itemsDisplayed;
+      };
+
+      $scope.addItems = function(){
+          $scope.config.itemsDisplayed += 15;
+          //$ionicScrollDelegate.resize();
+          //console.log($scope.route.steps.length + '  ' + $scope.config.itemsDisplayed);
+          $scope.$apply(function(){
+              $scope.$broadcast('scroll.infiniteScrollComplete');
+          });
+      };
+
+
+
       // **** Get polyline of map
       MapsSvc.promises.gMapsAPI.then(function(){ 
           
